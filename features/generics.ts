60d48d402290465ade669ebf976c71889e28f193
Robson Carvalho <robson01jc@gmail.com>
2022-08-19 12:11:31 -0300

    advanced generics

---
+++ features/generics.ts
@@ -0,0 +1,51 @@
+class ArrayOfAnything<T> {
+  constructor(public collection: T[]) {}
+
+  get(index: number): T {
+    return this.collection[index];
+  }
+}
+
+const arr = new ArrayOfAnything(['a', 'b', 'c']);
+
+// Example of generics with functions
+
+function printAnything<T>(arr: T[]): void {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+  }
+}
+
+printAnything<string>(['a', 'b', 'c']);
+
+// Generic Constraints
+
+function prop<T, K extends keyof T>(obj: T, key: K) {
+  return obj[key];
+}
+
+prop({ name: 'John', age: 29 }, 'age');
+
+class Car {
+  print() {
+    console.log('I am a car');
+  }
+}
+
+class House {
+  print() {
+    console.log('I am a house');
+  }
+}
+
+interface Printable {
+  print(): void;
+}
+
+function printHousesOrCars<T extends Printable>(arr: T[]): void {
+  for (let i = 0; i < arr.length; i++) {
+    arr[i].print();
+  }
+}
+
+printHousesOrCars<Car>([new Car(), new House()]);
